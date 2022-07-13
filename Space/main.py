9ed80da942f1130e0e6dbaf975d8efd6e44a9a4f
Astrikol <astrikol@gmail.com>
2022-07-13 14:49:08 +0300

    Added lasers and torpedoes to Space game. Changed fire mode to alternate between left and right lasers. Added counter for torpedoes.

--- Space/main.py
+++ Space/main.py
@@ -2,14 +2,15 @@ import pygame
 
 
 class MovingBackground:
+    """Create the moving stars in the background, simulating movement"""
     def __init__(self):
-        self.bgimage = pygame.image.load("./ASSETS/stars.png")
-        self.rectBGimg = self.bgimage.get_rect()
+        self.bg_image = pygame.image.load("./ASSETS/stars.png")
+        self.rect_BG_img = self.bg_image.get_rect()
 
         self.bgY1 = 0
         self.bgX1 = 0
 
-        self.bgY2 = self.rectBGimg.height
+        self.bgY2 = self.rect_BG_img.height
         self.bgX2 = 0
 
         self.movingUpSpeed = 1
@@ -17,75 +18,80 @@ class MovingBackground:
     def update(self):
         self.bgY1 += self.movingUpSpeed
         self.bgY2 += self.movingUpSpeed
-        if self.bgY1 >= self.rectBGimg.height:
-            self.bgY1 = -self.rectBGimg.height
-        if self.bgY2 >= self.rectBGimg.height:
-            self.bgY2 = -self.rectBGimg.height
+        if self.bgY1 >= self.rect_BG_img.height:
+            self.bgY1 = -self.rect_BG_img.height
+        if self.bgY2 >= self.rect_BG_img.height:
+            self.bgY2 = -self.rect_BG_img.height
 
     def render(self):
-        WINDOW.blit(self.bgimage, (self.bgX1, self.bgY1))
-        WINDOW.blit(self.bgimage, (self.bgX2, self.bgY2))
+        WINDOW.blit(self.bg_image, (self.bgX1, self.bgY1))
+        WINDOW.blit(self.bg_image, (self.bgX2, self.bgY2))
 
 
 class Player(pygame.sprite.Sprite):
+    """Main player class, responsible for the player's ship, movement and firing controls"""
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load("./ASSETS/player.png")
-        self.surf = pygame.Surface((50, 50))
-        self.rect = self.surf.get_rect(center=(480, 540))
+        self.rect = self.image.get_rect(center=(480, 540))
         self.last_shot = pygame.time.get_ticks()
-        self.shoot_delay = 250
-        self.lasers = []
-        self.torpedos = []
 
     def move(self):
         pressed_key = pygame.key.get_pressed()
         if pressed_key[pygame.K_w]:
-            self.rect.move_ip(0, -10)
+            self.rect.move_ip(0, -15)
         if pressed_key[pygame.K_s]:
-            self.rect.move_ip(0, 10)
+            self.rect.move_ip(0, 15)
         if pressed_key[pygame.K_a]:
-            self.rect.move_ip(-10, 0)
+            self.rect.move_ip(-15, 0)
         if pressed_key[pygame.K_d]:
-            self.rect.move_ip(10, 0)
+            self.rect.move_ip(15, 0)
         self.rect.clamp_ip(WINDOW_LIMIT)
 
-    def fire(self, keypress):
-        now = pygame.time.get_ticks()
-        laser = Projectiles(self.rect.centerx, self.rect.top)
-        torpedo = Projectiles(self.rect.centerx, self.rect.top)
-        if now - self.last_shot > self.shoot_delay:
-            self.last_shot = now
-        if keypress == pygame.K_SPACE:
-            self.lasers.append(laser)
-        if keypress == pygame.K_RCTRL:
-            self.torpedos.append(torpedo)
+    def create_projectile_laser(self, w_type):
+        self.last_shot = pygame.time.get_ticks()
+        return Projectiles(self.rect[0], self.rect[1], w_type)
 
 
 class Projectiles(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    """A class to handle lasers and torpedoes"""
+
+    fire_left = True
+
+    def __init__(self, x, y, w_type):
         super().__init__()
-        self.image = pygame.image.load("./ASSETS/laser.png")
-        self.torpedo_img = pygame.image.load("./ASSETS/torpedo.png")
-        self.rect = self.image.get_rect()
-        self.rect.bottom = y
-        self.rect.centerx = x
-        self.speedy = 250
+        self.type = w_type
+        if self.type == "laser":
+            self.image = pygame.image.load("./ASSETS/laser.png")
+            if Projectiles.fire_left:  # Alternate fire between left and right laser on the ship
+                self.rect = self.image.get_rect(center=(x + 22, y))
+                Projectiles.fire_left = False
+            else:
+                self.rect = self.image.get_rect(center=(x + 28, y))
+                Projectiles.fire_left = True
+        else:
+            self.image = pygame.image.load("./ASSETS/torpedo.png")
+            self.rect = self.image.get_rect(center=(x + 25, y))
 
     def update(self):
-        self.rect.y += self.speedy
+        self.rect.y -= 10
 
-        if self.rect.bottom < 0:
+        if self.rect.y < 0:
             self.kill()
 
 
 class Game:
+    """Main game class - everything runs from here"""
     def __init__(self):
         self.bg_animated = MovingBackground()
         self.pilot = Player()
-        self.all_sprites = pygame.sprite.Group()
-        self.all_sprites.add(self.pilot)
-        self.enemies = pygame.sprite.Group()
+        self.pilot_group = pygame.sprite.Group()
+        self.pilot_group.add(self.pilot)
+        self.enemies_group = pygame.sprite.Group()
+        self.projectiles_group = pygame.sprite.Group()
+        self.torpedoes = 3
+        self.font = pygame.font.Font("./ASSETS/crystal.ttf", 32)
+        self.text = self.font.render(str(self.torpedoes), True, "green", None)
 
     def run(self):
         run = True
@@ -93,18 +99,27 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
+
                 if event.type == pygame.KEYDOWN:  # Firing projectiles
-                    if event.key == pygame.K_SPACE:
-                        self.pilot.fire(pygame.K_SPACE)
+                    current_time = pygame.time.get_ticks()
+                    if event.key == pygame.K_SPACE and current_time - self.pilot.last_shot > 150:
+                        self.projectiles_group.add(self.pilot.create_projectile_laser("laser"))
+                    if event.key == pygame.K_RCTRL and self.torpedoes > 0:
+                        self.projectiles_group.add(self.pilot.create_projectile_laser(""))
+                        self.torpedoes -= 1
+                        self.text = self.font.render(str(self.torpedoes), True, "green", None)
 
             WINDOW.blit(BACKGROUND, (0, 0))
+            WINDOW.blit(TORPEDO_ICON, (40, 503))
+            WINDOW.blit(self.text, (10, 500))
             self.bg_animated.update()
             self.bg_animated.render()
 
-            for entity in self.all_sprites:
-                WINDOW.blit(entity.image, entity.rect)
-                entity.move()
+            self.projectiles_group.draw(WINDOW)
+            self.projectiles_group.update()
 
+            self.pilot_group.draw(WINDOW)
+            self.pilot.move()
 
             pygame.display.update()
             FramesPerSec.tick(FPS)
@@ -116,14 +131,16 @@ class Game:
 pygame.init()
 
 # Game global parameters
-FPS = 60
+FPS = 120
 FramesPerSec = pygame.time.Clock()
 WIDTH, HEIGHT = 960, 540
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 WINDOW_LIMIT = WINDOW.get_rect()
 BACKGROUND = pygame.image.load("./ASSETS/background.png")
-
+TORPEDO_ICON = pygame.image.load("./ASSETS/torpedo_icon.png")
+TORPEDO_COUNT = 3
 pygame.display.set_caption("Space game!")
+pygame.mouse.set_visible(False)
 
 game = Game()
 
